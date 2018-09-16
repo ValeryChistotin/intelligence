1c1f0b33482881281dfb62408011e62141ccd604
Valerushhh <Valerushhh@yandex.ru>
2018-09-16 22:08:49 +0300

    Move api to separate file

---
+++ helpers.js
@@ -0,0 +1,109 @@
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function givenFunction(x) {
+  return -(x * x) + 40 * x + 300;
+}
+
+function sumOfGivenFunction(argsArr) {
+  return argsArr.reduce((accumulator, currentX) => {
+    return accumulator + givenFunction(currentX);
+  });
+}
+
+function parseToBinary(number) {
+  return number.toString(2);
+}
+
+function parseToDecimal(number) {
+  return +number;
+}
+
+function adaptability(x, sum) {
+  return givenFunction(x) / sum;
+}
+
+function getPlan(x, sum) {
+  return adaptability(x, sum) * 4;
+}
+
+function crossover(firstNum, secondNum, breakPoint) {
+  firstNumFPath = firstNum.split('').slice(0, breakPoint);
+  firstNumSPath = firstNum.split('').slice(breakPoint);
+  secondNumFPath = secondNum.split('').slice(0, breakPoint);
+  secondNumSPath = secondNum.split('').slice(breakPoint);
+  return {
+    firstNum: firstNumFPath.join('') + secondNumSPath.join(''),
+    secondNum: secondNumFPath.join('') + firstNumSPath.join('')
+  };
+}
+
+function mutate(givenNumber, chance) {
+  var resultNumber = [];
+  var numberLength = givenNumber.length;
+
+  var mutatePoint = getRandomInt(0, numberLength);
+  var entryArray = givenNumber.split('').slice(mutatePoint);
+  var notMutatePath = givenNumber.slice(0, mutatePoint);
+
+  entryArray.every(item => {
+    if (Math.random() < chance) {
+      resultNumber.push(+!+item);
+
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  return notMutatePath + resultNumber.join('');
+}
+
+const getDecimal = num => {
+  let str = '' + num;
+  const zeroPos = str.indexOf('.');
+
+  if (zeroPos == -1) {
+    return 0;
+  }
+
+  str = str.slice(zeroPos);
+
+  return +str;
+};
+
+function mutate(givenNumber, chance) {
+  var resultNumber = [];
+  var numberLength = givenNumber.length;
+
+  var mutatePoint = getRandomInt(0, numberLength);
+  var entryArray = givenNumber.split('').slice(mutatePoint);
+  var notMutatePath = givenNumber.slice(0, mutatePoint);
+
+  entryArray.every(item => {
+    if (Math.random() < chance) {
+      resultNumber.push(+!+item);
+
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  return notMutatePath + resultNumber.join('');
+}
+
+module.exports = {
+  getRandomInt,
+  givenFunction,
+  sumOfGivenFunction,
+  parseToBinary,
+  parseToDecimal,
+  adaptability,
+  getPlan,
+  crossover,
+  // mutate,
+  getDecimal,
+  mutate
+};
--- index.js
+++ index.js
@@ -1,79 +1,19 @@
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+const helpers = require('./helpers');
 
-function givenFunction(x) {
-  return -(x * x) + 40 * x + 300;
-}
+const {
+  getRandomInt,
+  givenFunction,
+  sumOfGivenFunction,
+  parseToBinary,
+  parseToDecimal,
+  adaptability,
+  getPlan,
+  crossover,
+  mutate,
+  getDecimal
+} = helpers;
 
-function sumOfGivenFunction(argsArr) {
-  return argsArr.reduce((accumulator, currentX) => {
-    return accumulator + givenFunction(currentX);
-  });
-}
-
-function parseToBinary(number) {
-  return number.toString(2);
-}
-
-function parseToDecimal(number) {
-  return +number;
-}
-
-function adaptability(x, sum) {
-  return givenFunction(x) / sum;
-}
-
-function getPlan(x, sum) {
-  return adaptability(x, sum) * 4;
-}
-
-function crossover(firstNum, secondNum, breakPoint) {
-  firstNumFPath = firstNum.split('').slice(0, breakPoint);
-  firstNumSPath = firstNum.split('').slice(breakPoint);
-  secondNumFPath = secondNum.split('').slice(0, breakPoint);
-  secondNumSPath = secondNum.split('').slice(breakPoint);
-  return {
-    firstNum: firstNumFPath.join('') + secondNumSPath.join(''),
-    secondNum: secondNumFPath.join('') + firstNumSPath.join('')
-  };
-}
-
-function mutate(givenNumber, chance) {
-  var resultNumber = [];
-  var numberLength = givenNumber.length;
-
-  var mutatePoint = getRandomInt(0, numberLength);
-  var entryArray = givenNumber.split('').slice(mutatePoint);
-  var notMutatePath = givenNumber.slice(0, mutatePoint);
-
-  entryArray.every(item => {
-    if (Math.random() < chance) {
-      resultNumber.push(+!+item);
-
-      return true;
-    } else {
-      return false;
-    }
-  });
-
-  return notMutatePath + resultNumber.join('');
-}
-
-const getDecimal = num => {
-  let str = '' + num;
-  const zeroPos = str.indexOf('.');
-
-  if (zeroPos == -1) {
-    return 0;
-  }
-
-  str = str.slice(zeroPos);
-
-  return +str;
-};
-
-const indexes2 = {
+const indexes = {
   x: getRandomInt.bind(this, 0, 30),
   binary: parseToBinary,
   fx: givenFunction,
@@ -87,9 +27,9 @@ aggregation.chromosomes = [1, 2, 3, 4];
 
 const createField = (field, value, functionsSum) => {
   if (!aggregation[field]) {
-    aggregation[field] = [indexes2[field](value, functionsSum)];
+    aggregation[field] = [indexes[field](value, functionsSum)];
   } else {
-    aggregation[field].push(indexes2[field](value, functionsSum));
+    aggregation[field].push(indexes[field](value, functionsSum));
   }
 
   return aggregation[field][aggregation[field].length - 1];
@@ -122,26 +62,3 @@ aggregation.max = Math.max.apply(null, aggregation.fx);
 aggregation.middle = functionsSum / aggregation.chromosomes.length;
 
 console.log(aggregation);
-function mutate(givenNumber, chance){
-    var resultNumber = [];
-    var numberLength = givenNumber.length;
-
-    var mutatePoint = getRandomInt(0, numberLength);
-    var entryArray = givenNumber.split('').slice(mutatePoint);
-    var notMutatePath = givenNumber.slice(0, mutatePoint);
-
-    entryArray.every(item => {
-        if(Math.random() < chance){
-            resultNumber.push(+!+item);
-
-            return true;
-        } else {
-            return false;
-        }
-    });
-
-    return notMutatePath + resultNumber.join('');
-}
-
-console.log(mutate('10110110010111', 1));
-console.log(sumOfGivenFunction([1,5,12,30]));
