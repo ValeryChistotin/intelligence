8918245c0cd92afd36df21f939ebf446466f2815
artem <artem7753@mail.ru>
2018-09-17 15:39:01 +0300

    bug fixed

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const {
   getDecimal
 } = helpers;
 
+let max;
+
 const indexes = {
   x: getRandomInt.bind(this, 0, 30),
   binary: parseToBinary,
@@ -21,62 +23,79 @@ const indexes = {
   plan: getPlan,
   reality: value => value
 };
-const aggregation = {};
 
-aggregation.chromosomes = [
-  getRandomInt(0, 30),
-  getRandomInt(0, 30),
-  getRandomInt(0, 30),
-  getRandomInt(0, 30)
-];
+for (var i = 0; i < 6; i++) {
+  var aggregation = {};
 
-const createField = (field, value, functionsSum) => {
-  if (!aggregation[field]) {
-    aggregation[field] = [indexes[field](value, functionsSum)];
-  } else {
-    aggregation[field].push(indexes[field](value, functionsSum));
+  const createField = (field, value, functionsSum) => {
+    if (!aggregation[field]) {
+      aggregation[field] = [indexes[field](value, functionsSum)];
+    } else {
+      aggregation[field].push(indexes[field](value, functionsSum));
+    }
+
+    return aggregation[field][aggregation[field].length - 1];
+  };
+
+  if (!aggregation.chromosomes) {
+    aggregation.chromosomes = [
+      getRandomInt(0, 30),
+      getRandomInt(0, 30),
+      getRandomInt(0, 30),
+      getRandomInt(0, 30)
+    ];
   }
 
-  return aggregation[field][aggregation[field].length - 1];
-};
+  aggregation.chromosomes.forEach(value => {
+    createField('binary', value);
 
-aggregation.chromosomes.forEach(value => {
-  createField('binary', value);
+    const cur = createField('x');
+    createField('fx', cur);
+  });
 
-  const cur = createField('x');
-  createField('fx', cur);
-});
+  const functionsSum = sumOfGivenFunction(aggregation.x);
 
-const functionsSum = sumOfGivenFunction(aggregation.x);
+  aggregation.x.forEach(value => {
+    createField('adaptability', value, functionsSum);
+    const decimalPlan = createField('plan', value, functionsSum);
 
-aggregation.x.forEach(value => {
-  createField('adaptability', value, functionsSum);
-  const decimalPlan = createField('plan', value, functionsSum);
+    createField('reality', Math.round(decimalPlan));
+  });
 
-  createField('reality', Math.round(decimalPlan));
-});
+  let crossoverArr = crossover(
+    aggregation.binary[0],
+    aggregation.binary[1],
+    getRandomInt(0, 4)
+  ).concat(
+    crossover(aggregation.binary[2], aggregation.binary[3], getRandomInt(0, 5))
+  );
 
-let crossoverArr = crossover(
-  aggregation.binary[0],
-  aggregation.binary[1],
-  getRandomInt(0, 4)
-).concat(
-  crossover(aggregation.binary[2], aggregation.binary[3], getRandomInt(0, 5))
-);
+  let crossoveredValueInDec = crossoverArr.map(item => {
+    return parseToDecimal(item);
+  });
 
-let crossoveredValueInDec = crossoverArr.map(item => {
-  return parseToDecimal(item);
-});
+  let crossoveredFx = crossoveredValueInDec.map(item => {
+    return givenFunction(item);
+  });
 
-let crossoveredFx = crossoveredValueInDec.map(item => {
-  return givenFunction(item);
-});
+  aggregation.sum = functionsSum;
+  aggregation.middle = functionsSum / aggregation.chromosomes.length;
+  aggregation.crossover = crossoverArr;
+  aggregation.crossoveredValues = crossoveredValueInDec;
+  aggregation.crossoveredFx = crossoveredFx;
 
-aggregation.sum = functionsSum;
-aggregation.max = Math.max.apply(null, aggregation.fx);
-aggregation.middle = functionsSum / aggregation.chromosomes.length;
-aggregation.crossover = crossoverArr;
-aggregation.crossoveredValues = crossoveredValueInDec;
-aggregation.crossoveredFx = crossoveredFx;
+  if (max) {
+    var curMax = Math.max.apply(null, aggregation.fx);
+    max = max > curMax ? max : curMax;
+  } else {
+    max = Math.max.apply(null, aggregation.fx);
+  }
 
-console.log(aggregation);
+  console.log(aggregation);
+
+  aggregation = {};
+
+  aggregation.chromosomes = crossoveredValueInDec;
+}
+
+console.log(max);
