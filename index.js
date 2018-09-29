e6a4435784099cc9e19d8d52cde96906885c00f9
artem <artem7753@mail.ru>
2018-09-29 16:12:13 +0300

    fix

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ const training = {
 const correctAnswer = [0, 1, 0, 1, 0, 1, 0, 1, 0];
 
 const trainingFigures = [
-  '010110010010',
-  '111110100111',
-  '111110001110',
-  '011101111001',
-  '111011001111',
-  '001010111111',
-  '111011010100',
-  '111111101111',
-  '111111001111'
+  '010110010010', //1
+  '111110100111', //2
+  '111110001110', //3
+  '011101111001', //4
+  '111011001111', //5
+  '001010111111', //6
+  '111011010100', //7
+  '111111101111', //8
+  '111111001111' //9
 ];
 
 let weightCoeff = [];
@@ -23,7 +23,7 @@ let weightCoeff = [];
 for (let i = 0; i < 12; i += 1) {
   weightCoeff.push(Math.round(Math.random()));
 }
-//console.log(weightCoeff);
+console.log(weightCoeff);
 
 let errorSumm;
 let weightLimit = 2;
@@ -33,35 +33,34 @@ const iteration = () => {
   errorSumm = 0;
 
   trainingFigures.forEach((inputSignal, index) => {
-    let bitSumm;
-    let isCorrect;
+    let bitSumm = 0;
+    let isCorrect = 0;
     let answer;
 
-    bitSumm = 0;
-
-    //  console.log('input signal - ', inputSignal);
-    for (let i = 0; i < 10; i += 1) {
+    console.log('input signal - ', inputSignal);
+    for (let i = 0; i < 12; i += 1) {
       bitSumm += inputSignal[i] * weightCoeff[i];
     }
-    // console.log('bit summ - ', bitSumm);
+    console.log('bit summ - ', bitSumm);
 
     if (bitSumm > weightLimit) {
       answer = 1;
     } else {
       answer = 0;
     }
+    console.log('answer - ', answer, ' correct - ', correctAnswer[index]);
 
     isCorrect = answer - correctAnswer[index];
-    errorSumm += isCorrect;
-    // console.log('error sum- ', errorSumm);
+    errorSumm += isCorrect * isCorrect;
+    console.log('error sum- ', errorSumm);
 
     for (let i = 0; i < 12; i += 1) {
       weightCoeff[i] -= speedTraining * isCorrect * inputSignal[i];
     }
-    // console.log(weightCoeff);
+    console.log(weightCoeff);
 
     weightLimit += speedTraining * answer;
-    // console.log('weight limit - ', weightLimit, '\n');
+    console.log('weight limit - ', weightLimit, '\n');
   });
 };
 
@@ -76,7 +75,7 @@ while (errorSumm > extendsErrorCount) {
 
 console.log(weightCoeff);
 
-let signal = '010110010010';
+let signal = '111111101111';
 let bitSumm = 0;
 let answer;
 
